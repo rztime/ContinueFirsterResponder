12bb6932129bc4d7b382c3cdac380ee48b9c80d0
rztime <rztime@vip.qq.com>
2017-11-13 14:13:28 +0800

    iOS 界面刷新时 保持UITextField UITextView UISearchBar 响应，保持键盘一直打开，并可在刷新后指定定位到某输入框作为第一响应者
    iOS 界面刷新时 保持UITextField UITextView UISearchBar 响应，保持键盘一直打开，并可在刷新后指定定位到某输入框作为第一响应者

---
+++ TextInputViewFocusDemo/TextInputViewFocusDemo/TestField.m
@@ -0,0 +1,26 @@
+//
+//  TestField.m
+//  TextInputViewFocusDemo
+//
+//  Created by rztime on 2017/11/13.
+//  Copyright © 2017年 rztime. All rights reserved.
+//
+
+#import "TestField.h"
+#import "UIView+RZContinueFirstResponder.h"
+
+@implementation TestField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)dealloc {
+    NSLog(@"%@ __ %s", self.tagIdentity, __FUNCTION__);
+}
+
+@end
